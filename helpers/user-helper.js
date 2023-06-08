419015026b521f90441a426890cd76f8f1560a6f
Fibin Johnson <128117075+Fibinjohnson@users.noreply.github.com>
2023-06-08 22:56:15 +0530

    third commit

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -48,33 +48,84 @@ doLogin: (userLogin) => {
       }
     });
   },
-  addProductsCart:((userId,userProducts)=>{return new Promise(async(resolve,reject)=>{
+  addProductsCart:(userId,userProducts)=>{return new Promise(async(resolve,reject)=>{
+    console.log("useridf:" ,userId)
      const cart={
           user:new ObjectId(userId),
           product:[new ObjectId(userProducts)]
     }
     
-    const database=await connectToDB();
-     const userCart=  database.collection("newCart").findOne({user:new ObjectId(userId)})
-       database.collection("newCart").find().toArray().then((res)=>console.log(res))
-    
-    if(userCart){
-      
-      const collect=   database.collection("newCart")
-     
-         collect.updateOne({user:new ObjectId(userId)},{
-         $push:{product:new ObjectId(userProducts)}
-        }
-      ).then((response)=>resolve(response));
-    }else {
-      
-       await database.collection("newCart").insertOne(cart).then((res)=>{resolve(res)})
-      
+const database=await connectToDB();
+const userCart = await database.collection("newCart").findOne({ user: new ObjectId(userId) });
+console.log('Before find toArray');
+database.collection("newCart").find().toArray().then((res) => console.log(res));
+console.log('After find toArray');
+
+if (userCart) {
+  console.log('Updating existing document');
+  const collect = database.collection("newCart");
+  collect.updateOne(
+    { user: new ObjectId(userId) },
+    {
+      $push: { product: new ObjectId(userProducts) }
     }
-    
+  ).then((response) => resolve(response));
+} else {
+  console.log('Inserting new document');
+  await database.collection("newCart").insertOne(cart).then((res) => {
+    resolve(res);
+  });
+}  
   }
-  )}
-  )
+  )},
+  AddToUsercart:(userId)=>
+  {
+    return new Promise(async(resolve,reject)=>{
+      try{
+      const database=await connectToDB();
+      const userCartData=await database.collection("newCart").aggregate([
+        {
+          $match:{user: new ObjectId(userId) }
+        },
+       {
+         $lookup:{
+          from:collection.COLLECTION_NAME,
+          let:{prodList:"$product"},
+          pipeline:[
+            {
+              $match:{
+                $expr:{
+                  $in:["$_id","$$prodList"]
+                }
+              }
+            }
+          ],as:"CartItem"
+        }
+      }
+      ]).toArray()
+      console.log(userCartData,"this")
+      resolve(userCartData[0].CartItem)
+     } catch (error) {
+        reject(error);
+      }              
+
+  }
+    )
+  },
+   getCartCount:(userId)=>{
+    return new Promise(async(resolve,reject)=>{
+      const database= await connectToDB();
+      let Count=0;
+      const cartCount=await database.collection("newCart").findOne({user:new ObjectId(userId)});
+      if(cartCount){
+        Count=cartCount.product.length;
+        resolve(Count)
+      }
+
+    })
+   }
+  
+
 
   
   
--- routes/user.js
+++ routes/user.js
@@ -1,63 +1,72 @@
 var express = require('express');
 var router = express.Router();
-var products=require("../helpers/product-herpers")
-var userSignup=require('../helpers/user-helper');
+var products = require("../helpers/product-herpers")
+var userSignup = require('../helpers/user-helper');
 
 const userHelper = require('../helpers/user-helper');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
- products.listAdminProducts().then((product)=>{
-  let user=req.session.user
-  res.render('users/index', { product ,user})
-  
- })
-  
- 
+router.get('/', async function (req, res, next) {
+  await products.listAdminProducts().then(async (product) => {
+    let user = req.session.user
+    if (user) {
+      count = await userHelper.getCartCount(user._id)
+    }
+    res.render('users/index', { product, user, count })
+  })
 });
-router.get("/login",(req,res)=>{
-  if(req.session.loggedIn) {
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
     res.redirect('/')
-  }else
-  res.render("users/login",{"error":req.session.logError})
-  req.session.logError=false
-  
+  } else
+    res.render("users/login", { "error": req.session.logError })
+  req.session.logError = false
+
 
 }
 )
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
   res.render("users/signup")
 }
 )
-router.post('/signup',(req,res)=>{
-  userSignup.doSignup(req.body).then((data)=>console.log(data), res.redirect("/"))
-  
+router.post('/signup', (req, res) => {
+  userSignup.doSignup(req.body).then((data) => console.log(data), res.redirect("/login"))
+
 
 })
-router.post("/login",(req,res)=>{
-  userSignup.doLogin(req.body).then((data)=>{
-    if(data.status){
-      req.session.loggedIn=true;
-      req.session.user=data.user;
+router.post("/login", (req, res) => {
+  userSignup.doLogin(req.body).then((data) => {
+    if (data.status) {
+      console.log('Setting req.session.loggedIn');
+      req.session.loggedIn = true;
+      console.log('Setting req.session.user');
+      req.session.user = data.user;
+      console.log('req.session.user:', req.session.user);
+      console.log(data.user);
       res.redirect("/")
-  }
-   else{
-    req.session.logError="Invalid username or Password";
-    res.redirect("/login")
-   
-    
-   }})
+    }
+    else {
+      req.session.logError = "Invalid username or Password";
+      res.redirect("/login")
+
+
+    }
+  })
 })
-router.get('/logout',(req,res)=>{
+router.get('/logout', (req, res) => {
   req.session.destroy();
   res.redirect("/login")
 })
-router.get("/cart",(req,res)=>{
-  res.render('users/cart')
-})
-router.get('/Add-Products-Cart/:id',(req,res)=>{
-  
-  userHelper.addProductsCart(req.session.user._id,req.params.id).then(()=>{
+router.get("/cart", (async (req, res) => {
+  const cart = await userHelper.AddToUsercart(req.session.user._id);
+  console.log(cart, "oomb")
+  let user = req.session.user
+  res.render('users/cart', { user, cart })
+}))
+router.get('/Add-Products-Cart/:id', async (req, res) => {
+
+  await userHelper.addProductsCart(req.session.user._id, req.params.id).then(() => {
+    console.log(req.session.user._id, req.params.id);
     res.redirect("/")
   })
 })
